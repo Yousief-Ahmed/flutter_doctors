b3b340e0eeecb9ae54dab049e23ca98b9cc6e032
Yousief Ahmed <109359820+Yousief-Ahmed@users.noreply.github.com>
2024-10-16 23:46:49 +0300

    - Added Development and Production flavors - changed App name (Android version ) based on each flavor - Added two (main) functions for each flavor

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,7 +10,7 @@ import 'package:flutter_doctors/core/routing/app_router.dart';
 import 'package:flutter_doctors/doc_app.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_doctors/main.dart';
+import 'package:flutter_doctors/main_development.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
